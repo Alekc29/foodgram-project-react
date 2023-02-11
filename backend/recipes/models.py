c8282613d506e5aa96692f66c10a7a874b9598fe
alex <alex@debian.alex.com>
2023-02-11 21:51:28 +0300

    Foodgram dev. Add models tag.

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,55 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        'Название',
+        unique=True,
+        max_length=200
+    )
+    color_hex = models.CharField(
+        'Цветовой HEX-код',
+        unique=True,
+        max_length=7,
+        validators=[
+            RegexValidator(
+                regex='^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$',
+                message='Введенное значение не является цветом в формате HEX!'
+            )
+        ]
+    )
+    slug = models.SlugField(
+        'Уникальный слаг',
+        unique=True,
+        max_length=200
+    )
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        related_name='recipes',
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name='Автор публикации',
+    )
+    title = models.CharField('Название', max_length=200)
+    image = models.ImageField(
+        'Картинка',
+        upload_to='recipes/'
+    )
+    description = models.TextField('Текстовое описание')
+    ingredients = models.TextField()
+    tags = models.ManyToManyField(
+        Tag,
+        related_name='recipes',
+        verbose_name='Тег'
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        'Время приготовления в минутах',
+        validators=[MinValueValidator(1, message='Минимальное значение 1!')]
+    )
