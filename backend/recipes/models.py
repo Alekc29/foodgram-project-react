9f78ae360dc2c1e28304f7ec8b1bea67633b69de
alex <alex@debian.alex.com>
2023-02-12 21:12:29 +0300

    Foodgram dev. Add models ingradient.

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -11,16 +11,14 @@ class Tag(models.Model):
         unique=True,
         max_length=200
     )
-    color_hex = models.CharField(
+    color = models.CharField(
         'Цветовой HEX-код',
         unique=True,
         max_length=7,
-        validators=[
-            RegexValidator(
-                regex='^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$',
-                message='Введенное значение не является цветом в формате HEX!'
-            )
-        ]
+        validators=[RegexValidator(
+            regex='^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$',
+            message='Введенное значение не является цветом в формате HEX!'
+        )]
     )
     slug = models.SlugField(
         'Уникальный слаг',
@@ -29,6 +27,17 @@ class Tag(models.Model):
     )
 
 
+class Ingredient(models.Model):
+    name = models.CharField('Название', max_length=200)
+    measurement_unit = models.CharField('Единицы измерения', max_length=200)
+    amount = models.PositiveSmallIntegerField(
+        verbose_name='Количество',
+        validators=[
+            MinValueValidator(1, message='Количество должно быть больше 0!')
+        ]
+    )
+    
+    
 class Recipe(models.Model):
     author = models.ForeignKey(
         User,
@@ -37,13 +46,17 @@ class Recipe(models.Model):
         null=True,
         verbose_name='Автор публикации',
     )
-    title = models.CharField('Название', max_length=200)
+    name = models.CharField('Название', max_length=200)
     image = models.ImageField(
         'Картинка',
         upload_to='recipes/'
     )
-    description = models.TextField('Текстовое описание')
-    ingredients = models.TextField()
+    text = models.TextField('Текстовое описание')
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        related_name='recipes',
+        verbose_name='Ингредиенты'
+    )
     tags = models.ManyToManyField(
         Tag,
         related_name='recipes',
@@ -53,3 +66,11 @@ class Recipe(models.Model):
         'Время приготовления в минутах',
         validators=[MinValueValidator(1, message='Минимальное значение 1!')]
     )
+
+    @property
+    def is_favorited(self):
+        return
+    
+    @property
+    def is_in_shopping_cart(self):
+        return
