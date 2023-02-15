69a44208b0d89f0fa8d60a89cc4172c7e488f57d
alex <alex@debian.alex.com>
2023-02-15 19:34:33 +0300

    Foodgram dev. Fix models users.

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 
 class User(AbstractUser):    
@@ -28,6 +29,16 @@ class User(AbstractUser):
         blank=True,
     )
 
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('username', 'email'),
+                name='unique_user'
+            )
+        ]
+
     def __str__(self):
         return self.username
     
@@ -45,3 +56,20 @@ class Follow(models.Model):
         related_name='following',
         verbose_name='Автор'
     )
+
+    def __str__(self):
+        return f'Автор: {self.author}, подписчик: {self.user}'
+
+    def save(self, **kwargs):
+        if self.user == self.author:
+            raise ValidationError("Невозможно подписаться на себя")
+        super().save()
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'user'],
+                name='unique_follower')
+        ]
