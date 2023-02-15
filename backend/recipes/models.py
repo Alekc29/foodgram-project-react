e228225783a50420645476ed9eb0d7fa51e4185b
alex <alex@debian.alex.com>
2023-02-15 19:49:41 +0300

    Foodgram dev. Add models favorite.

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
+from django.db.models import UniqueConstraint
 
 User = get_user_model()
 
@@ -26,10 +27,25 @@ class Tag(models.Model):
         max_length=200
     )
 
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return f'{self.name}'
+
+
 
 class Ingredient(models.Model):
     name = models.CharField('Название', max_length=200)
     measurement_unit = models.CharField('Единицы измерения', max_length=200)
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+
+    def __str__(self):
+        return f'{self.name}, {self.measurement_unit}'
     
     
 class Recipe(models.Model):
@@ -79,3 +95,45 @@ class IngredientInRecipe(models.Model):
         'Количество',
         validators=[MinValueValidator(1, message='Минимальное количество 1!')]
     )
+
+    class Meta:
+        verbose_name = 'Количество ингредиента'
+        verbose_name_plural = 'Количество ингредиентов'
+        constraints = [
+            UniqueConstraint(
+                fields=('recipe', 'ingredient'),
+                name='unique ingredient for recipe'
+            )
+        ]
+
+    def __str__(self):
+        return (f'{self.recipe}: {self.ingredient.name},'
+                f' {self.amount}, {self.ingredient.measurement_unit}')
+    
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,        
+        related_name='favorite',
+        verbose_name='Пользователь',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,        
+        related_name='favorite',
+        verbose_name='Рецепты',
+    )
+
+    class Meta:
+        verbose_name = 'Избранный рецепт'
+        verbose_name_plural = 'Избранные рецепты'
+        constraints = (
+            UniqueConstraint(
+                fields=('user', 'recipe'),
+                name='unique favorite'
+            ),
+        )
+
+    def __str__(self):
+        return f'{self.recipe} в избранном у {self.user}'
