1b019ce4c6634ee64b3d62e3bf62f52e73b9d44a
alex <alekco29@yandex.ru>
2023-03-12 22:58:03 +0300

    Fix views tags create all.

--- backend/api/views.py
+++ backend/api/views.py
@@ -95,6 +95,10 @@ class TagViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = (AllowAny,)
     pagination_class = None
 
+    @action(detail=False)
+    def create(self, request):
+        return Tag.objects.all()
+
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """Вьюсет для работы с рецептами.
